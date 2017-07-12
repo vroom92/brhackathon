935579b4d45b1ae4c1f4a101fe0e3e5fec262161
vroom92 <varunagr@usc.edu>
2017-07-12 11:05:31 -0700

    added weather

--- index.js
+++ index.js
@@ -36,9 +36,32 @@ restService.post('/hook', function (req, res) {
                     speech += '';
 					}
 				} else if( requestBody.result.action == "get.dev.cert.info" ) {
-                    speech = "See: http://webster.bfm.com/Wiki";
+                    speech+ = "See: http://webster.bfm.com/Wiki";
 
                 }
+				 else if (requestBody.result.action == "weather") {
+                    var city = req.body.result.parameters['geo-city'];
+                   // console.log(city);
+                    if (req.body.result.parameters['date']) {
+                        var date = req.body.result.parameters['date'];
+                        //console.log('Date: ' + date);
+                    }
+					var url = "https://api.worldweatheronline.com/premium/v1/weather.ashx?format=json&num_of_days=1&"+"q="+city+"&key=9b586ac440a244c0bbd205511171107&date=today";
+					var xmlhttp = new XMLHttpRequest();
+					xmlhttp.onreadystatechange = function() {
+						if (this.readyState == 4 && this.status == 200) {
+							var response=(JSON.parse(this.responseText));
+							var forecast = response['data']['weather'][0];
+                            conditions = response['data']['current_condition'][0];
+                            currdate = forecast['date'];
+                            currTemp = conditions['temp_F'];
+							speech +=requestBody.result.fulfillment.speech;
+							speech+=" "+currTemp+" on "+currdate;
+						}
+					};
+					xmlhttp.open("GET",url, true);
+					xmlhttp.send();
+				 }	
             }
         }
 
